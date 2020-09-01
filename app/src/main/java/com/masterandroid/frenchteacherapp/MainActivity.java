3b34ff8a65d3d57fb141d0c8589c2465a23b96b4
Jhalak <suhanejhalak@gmail.com>
2020-09-01 07:14:05 -0400

    French Teacher App

---
+++ app/src/main/java/com/masterandroid/frenchteacherapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.masterandroid.frenchteacherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sayTheColor(View view)
+    {
+        Button buttonclicked = (Button)view;
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,
+                getResources().getIdentifier(buttonclicked.getTag().toString(),
+                        "raw",
+                        getPackageName()));
+        mediaPlayer.start();
+    }
+}
